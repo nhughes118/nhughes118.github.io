138afdf4a31580e40479e2df448f567038e32ec9
Nick Hughes <nkhughes@outlook.com>
2020-02-03 20:39:45 -0500

    adjusted font and nav bar

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ $(document).ready(function(){
 	$('#top').click(function(){
 		$('html,body').animate({scrollTop: 0}, 500);
 	});
+	
+	$('.mobileMenu').click(function(){
+		$('#topNav li').toggle("slide");
+	});
 
 
 }); // closes my document.ready
